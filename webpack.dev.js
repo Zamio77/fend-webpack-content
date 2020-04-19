c9e5938d64e66a02bf96227bdc3520dbd6ff40ec
Zamio77 <38842486+Zamio77@users.noreply.github.com>
2020-04-19 17:44:49 -0400

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,8 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "production",
@@ -29,5 +31,6 @@ module.exports = {
       cleanStaleWebpackAssets: true,
       protectWebpackAssets: false,
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
